92f41995963572fbb419d4f08a6e5a2f379820c8
akumetan <88800946+akumetan@users.noreply.github.com>
2026-08-31 01:53:56 +0200

    Init base setup

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("java-library")
+}
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:26.2.build.+")
+}
+
+java {
+    toolchain.languageVersion = JavaLanguageVersion.of(25)
+}
+
+tasks {
+    processResources {
+        val props = mapOf("version" to version , "description" to project.description )
+        filesMatching("plugin.yml") {
+            expand(props)
+        }
+    }
+}
