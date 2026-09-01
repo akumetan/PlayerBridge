87aeff2a5c78265a9429cb208550b1335a2d80b2
akumetan <88800946+akumetan@users.noreply.github.com>
2026-09-01 02:31:09 +0200

    Rename file to yml

--- src/main/java/io/github/akumetan/playerbridge/config/ConfigManager.java
+++ src/main/java/io/github/akumetan/playerbridge/config/ConfigManager.java
@@ -16,23 +16,23 @@ public class ConfigManager {
     public void loadConfiguration() {
         plugin.saveDefaultConfig();
         plugin.reloadConfig();
-        FileConfiguration file = plugin.getConfig();
+        FileConfiguration yml = plugin.getConfig();
 
         var database = new DatabaseConfig(
-                file.getString("database.host", "127.0.0.1"),
-                file.getInt("database.port", 3306),
-                file.getString("database.database", "player_bridge"),
-                file.getString("database.username", "root"),
-                file.getString("database.password", ""),
-                file.getInt("database.max-pool-size", 10),
-                file.getInt("database.min-pool-size", 2),
-                file.getInt("database.connection-timeout-ms", 5000)
+                yml.getString("database.host", "127.0.0.1"),
+                yml.getInt("database.port", 3306),
+                yml.getString("database.database", "player_bridge"),
+                yml.getString("database.username", "root"),
+                yml.getString("database.password", ""),
+                yml.getInt("database.max-pool-size", 10),
+                yml.getInt("database.min-pool-size", 2),
+                yml.getInt("database.connection-timeout-ms", 5000)
         );
 
         var messages = new MessagesConfig(
-                file.getString("messages.no-permission", "<red>You do not have permission to use this command.</red>"),
-                file.getString("messages.reload-config", "<green>Configuration successfully reloaded.</green>"),
-                file.getString("messages.check-version", "<gray>You are running version <white><version></white> (<white><server_version></white>).</gray>")
+                yml.getString("messages.no-permission", "<red>You do not have permission to use this command.</red>"),
+                yml.getString("messages.reload-config", "<green>Configuration successfully reloaded.</green>"),
+                yml.getString("messages.check-version", "<gray>You are running version <white><version></white> (<white><server_version></white>).</gray>")
         );
 
         this.config = new PlayerBridgeConfig(database, messages);
